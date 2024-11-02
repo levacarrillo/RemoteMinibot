8ded30aa66a60b7e4c33a4af10b69902a4e3ad50
Luis Nava <levacarrillo@gmail.com>
2024-11-02 00:13:42 -0600

    Add nodes for lases and light sensors in hardware pkg

--- catkin_ws/src/hardware/src/laser_scan_node.cpp
+++ catkin_ws/src/hardware/src/laser_scan_node.cpp
@@ -1,109 +1,72 @@
 #include <ros/ros.h>
-#include <sensor_msgs/LaserScan.h>
-#include <tf/transform_listener.h>
 #include <std_msgs/Int16MultiArray.h>
+#include <sensor_msgs/LaserScan.h>
 
+#define first_sensor_id 2
+#define last_sensor_id 5
 
-using namespace std;
+// CONSTANTS TO MODEL DATA
+#define a 9.609
+#define b -0.832
+#define c 0.08
 
+float sharp_distance[3];
 
-vector<int> sample;
-double sharp_distance[3];
-const int num_samples = 8;
-vector<vector<int>> samples;
-int sum_samples[3] = {0, 0, 0};
-
-void sharp_callback(const std_msgs::Int16MultiArray::ConstPtr& msg) {
-
-	for(int i=2; i<5; i++) sample.push_back(msg->data[i]);
-
-	samples.push_back(sample);
-	sample.clear();
-
-	if(samples.size() > num_samples) {
-	    samples.erase(samples.begin());
-	}
-	
-	for(int i=0; i<samples.size(); i++) 
-	    for(int j=0; j<samples[i].size(); j++) sum_samples[j] += samples[i][j];
-
-	for(int i=0; i<3; i++){
-	    sharp_distance[i] = 9.609  * pow(sum_samples[i]/samples.size(), -0.832) + 0.08;
-	    if(sharp_distance[i] > 0.7) sharp_distance[i] = 0.7;
-	    sum_samples[i] = 0;
-	}
+void subs_callback(const std_msgs::Int16MultiArray::ConstPtr& msg) {
+    for(size_t i=first_sensor_id; i<last_sensor_id; i++) {
+        int sharp_reading = msg->data[i];
+        sharp_distance[i-first_sensor_id] = a * pow(sharp_reading, b) + c;
+        // std::cout << "sharp_distance: " << i << " -> " << sharp_distance[i-first_sensor_id] << std::endl;
+        if(sharp_distance[i - first_sensor_id] > 0.7) {
+            sharp_distance[i - first_sensor_id] = 0.7;
+        }
+    }
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "laser_scan_node");
+    ros::NodeHandle nh;
+    ros::Rate loop(50);
 
-	cout << "Starting laser_scan_node by Luis Näva..." << endl;
+    ros::Publisher pub = nh.advertise<sensor_msgs::LaserScan>("/scan", 50);
+    ros::Subscriber subs = nh.subscribe<std_msgs::Int16MultiArray>("/sharp_sensors", 1, subs_callback);
+    
+    unsigned int num_readings = 3;
+    double laser_frequency = 40;
+    double ranges[num_readings];
+    double intensities[num_readings];
 
-	ros::init(argc, argv, "laser_scan_node");
-	ros::NodeHandle n;
+    while(ros::ok()) {
 
-	ros::Publisher  scan_pub = n.advertise<sensor_msgs::LaserScan>("/scan", 50);
-	ros::Subscriber laser_sub = n.subscribe("/sharp_sensors", 100, sharp_callback); 
+        for(size_t i=0; i<num_readings; i++) {
+            ranges[i] = sharp_distance[i];
+            intensities[i] = 100;
+        }
 
-	unsigned int num_readings = 3;
-	double laser_frequency = 40;
-	double ranges[num_readings];
-	double intensities[num_readings];
-	double theta = 0.5235;
+        ros::Time scan_time = ros::Time::now();
+        sensor_msgs::LaserScan scan;
+        scan.header.stamp = scan_time;
+        scan.header.frame_id = "laser_link";
+        scan.angle_min = - M_PI / 4;
+        scan.angle_max = M_PI;
 
-	int count = 0;
-	ros::Rate loop(50);
+        scan.angle_increment = M_PI / 4;
+        scan.time_increment = 1 / (laser_frequency * num_readings);
+        scan.range_min = 0.0;
+        scan.range_max = 100.0;
 
-	tf::TransformListener listener;
+        scan.ranges.resize(num_readings);
+        scan.intensities.resize(num_readings);
 
+        for(unsigned int i=0; i< num_readings; ++i) {
+            scan.ranges[i] = ranges[i];
+            scan.intensities[i] = intensities[i];
+        }
 
-	while(n.ok()){
+        pub.publish(scan);
+        ros::spinOnce();
+        loop.sleep();
+    }
 
-		tf::StampedTransform transform;
-		try {
-
-			listener.waitForTransform("base_link", "laser_link", ros::Time(0), ros::Duration(10.0));
-			listener.lookupTransform("base_link", "laser_link", ros::Time(0), transform);
-		}
-
-		catch (tf::TransformException ex){
-
-			ROS_ERROR("%s",ex.what());
-			ros::Duration(1.0).sleep();
-		}
-
-		for(int i=2; i<5; i++) {
-			ranges[i-2] = sharp_distance[i-2];
-			intensities[i-2] = 100;
-		}
-
-		ros::Time scan_time = ros::Time::now();
-
-		sensor_msgs::LaserScan scan;
-		scan.header.stamp = scan_time;
-		scan.header.frame_id = "laser_link";
-		//scan.angle_min = - 2.35619;
-		scan.angle_min = - 0.7853;
-		//scan.angle_max =    3.1416;
-		scan.angle_max = 3.1416;
-
-		scan.angle_increment = 0.785398;
-		scan.time_increment = (1 / laser_frequency) / (num_readings);
-		scan.range_min = 0.0;
-		scan.range_max = 100.0;
-
-		scan.ranges.resize(num_readings);
-		scan.intensities.resize(num_readings);
-
-		for(unsigned int i = 0; i < num_readings; ++i) {
-
-			scan.ranges[i] = ranges[i];
-			scan.intensities[i] = intensities[i];
-		}
-
-		scan_pub.publish(scan);
-		++count;
-
-		ros::spinOnce();
-		loop.sleep();
-	}
+    return 0;
 }
---
+++ catkin_ws/src/hardware/src/light_sensors_node.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <std_msgs/Int16MultiArray.h>
+#include <hardware/LightReadings.h>
+
+float light_readings[8];
+
+bool light_callback(hardware::LightReadings::Request &req, hardware::LightReadings::Response &res) {
+    int sensor_max_intensity = 0;
+    float max_intensity = 0.0f;
+
+    for(size_t i=0; i<8; i++) {
+        res.light_readings[i] = light_readings[i];
+        if (light_readings[i] > max_intensity) {
+            max_intensity = light_readings[i];
+            sensor_max_intensity = i;
+        }
+    }
+    res.sensor_max_intensity = sensor_max_intensity;
+    res.max_intensity = max_intensity;
+    return true;
+}
+
+void subs_callback(const std_msgs::Int16MultiArray::ConstPtr& msg) {
+    for(size_t i=0; i<8; i++) {
+        light_readings[i] = msg->data[i];
+        // std::cout << "light_readings: " << i << " -> " << light_readings[i] << std::endl;
+    }
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "light_sensors_node");
+    ros::NodeHandle nh;    
+
+    ros::ServiceServer service = nh.advertiseService("/light_readings", light_callback);
+    ros::Subscriber subs = nh.subscribe<std_msgs::Int16MultiArray>("/light_sensors", 1, subs_callback);
+    ros::spin();
+    return 0;
+}
